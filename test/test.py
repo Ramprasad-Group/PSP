06634d1d37572499169ce1153b488b009a32e6cd
Huan Tran <huantd@gmail.com>
2020-07-12 21:24:42 -0400

    readvasp fixed

--- test/test.py
+++ test/test.py
@@ -1,19 +1,24 @@
-import pandas as pd
-import PSP.PolymerBuilder as PB
+#import pandas as pd
+#import PSP.PolymerBuilder as PB
 import PSP.CrystalBuilder as CB
 
-df_smiles = pd.read_csv('test.csv', low_memory=False)   # fingerprinted data
+#df_smiles = pd.read_csv('test.csv', low_memory=False)   # fingerprinted data
 #df_smiles=df_smiles[['ID','smiles']]
 
-chain_builder = PB.Builder(df_smiles,ID_col='PID',SMILES_col='smiles_polymer', num_conf=1, Steps=25, Substeps=10, input_monomer_angles='medium', input_dimer_angles='medium', method='SA', n_cores=0)
-results = chain_builder.BuildPolymer()
-print(results)
+#chain_builder = PB.Builder(df_smiles,ID_col='PID', SMILES_col='smiles_polymer', 
+#        num_conf=1, Steps=25, Substeps=10, input_monomer_angles='medium', 
+#        input_dimer_angles='medium', method='SA', n_cores=0)
+#results = chain_builder.BuildPolymer()
+#print('>>>', results)
 #exit()
-ID='P070459'
+
+ID='P3'
 import glob
 try:
     list = glob.glob("Single-Chains/"+ID+'/'+"*.vasp")
-    crystal = CB.Builder(VaspInp_list=list, Nsamples=5, Input_radius='auto', OutDir='Crystals/')
+    print (list)
+    crystal = CB.Builder(VaspInp_list=list, Nsamples=5, Input_radius='auto', 
+            OutDir='Crystals/')
     results = crystal.BuildCrystal()
     print(results)
 except:
